7b5a43061f322ecd98a6b06009bc88c9328fc087
jaewoong <ys05059@gmail.com>
2025-09-04 09:09:23 +0900

    [UNITY] Feat: 플레이어 이동, 화면 스크롤 구현 (S13P21D103-25)

--- unity/Assets/Scripts/ControlPanel/VirtualInputs/UIVirtualTouchZone.cs
+++ unity/Assets/Scripts/ControlPanel/VirtualInputs/UIVirtualTouchZone.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class UIVirtualTouchZone : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+public class UIVirtualTouchZone
+    : MonoBehaviour,
+        IPointerDownHandler,
+        IDragHandler,
+        IPointerUpHandler
 {
     [System.Serializable]
     public class Event : UnityEvent<Vector2> { }
@@ -17,12 +21,16 @@ public class UIVirtualTouchZone : MonoBehaviour, IPointerDownHandler, IDragHandl
     public bool invertXOutputValue;
     public bool invertYOutputValue;
 
-    //Stored Pointer Values
+    // 터치 위치 저장
     private Vector2 pointerDownPosition;
     private Vector2 currentPointerPosition;
 
     [Header("Output")]
     public Event touchZoneOutputEvent;
+
+    [Header("Touch Events")]
+    public UnityEvent<Vector2> touchStartEvent;
+    public UnityEvent touchEndEvent;
 
     void Start()
     {
@@ -31,18 +39,27 @@ public class UIVirtualTouchZone : MonoBehaviour, IPointerDownHandler, IDragHandl
 
     private void SetupHandle()
     {
-        if(handleRect)
+        if (handleRect)
         {
-            SetObjectActiveState(handleRect.gameObject, false); 
+            SetObjectActiveState(handleRect.gameObject, false);
         }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            containerRect,
+            eventData.position,
+            eventData.pressEventCamera,
+            out pointerDownPosition
+        );
 
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(containerRect, eventData.position, eventData.pressEventCamera, out pointerDownPosition);
+        currentPointerPosition = pointerDownPosition;
 
-        if(handleRect)
+        // 터치 시작 이벤트 발생 (절대 위치)
+        touchStartEvent.Invoke(pointerDownPosition);
+
+        if (handleRect)
         {
             SetObjectActiveState(handleRect.gameObject, true);
             UpdateHandleRectPosition(pointerDownPosition);
@@ -51,16 +68,32 @@ public class UIVirtualTouchZone : MonoBehaviour, IPointerDownHandler, IDragHandl
 
     public void OnDrag(PointerEventData eventData)
     {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            containerRect,
+            eventData.position,
+            eventData.pressEventCamera,
+            out currentPointerPosition
+        );
 
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(containerRect, eventData.position, eventData.pressEventCamera, out currentPointerPosition);
-        
-        Vector2 positionDelta = GetDeltaBetweenPositions(pointerDownPosition, currentPointerPosition);
+        // 터치 시작점으로부터의 누적 이동량 계산 (절대 오프셋)
+        Vector2 touchOffset = GetDeltaBetweenPositions(pointerDownPosition, currentPointerPosition);
 
-        Vector2 clampedPosition = ClampValuesToMagnitude(positionDelta);
-        
-        Vector2 outputPosition = ApplyInversionFilter(clampedPosition);
+        // 클램핑 적용 (선택사항)
+        Vector2 processedOffset = clampToMagnitude
+            ? ClampValuesToMagnitude(touchOffset)
+            : touchOffset;
 
+        // 반전 필터 적용
+        Vector2 outputPosition = ApplyInversionFilter(processedOffset);
+
+        // 연속 입력으로 전달 (누적 오프셋)
         OutputPointerEventValue(outputPosition * magnitudeMultiplier);
+
+        // 핸들 위치 업데이트 (시각적 피드백)
+        if (handleRect)
+        {
+            UpdateHandleRectPosition(processedOffset);
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -68,9 +101,13 @@ public class UIVirtualTouchZone : MonoBehaviour, IPointerDownHandler, IDragHandl
         pointerDownPosition = Vector2.zero;
         currentPointerPosition = Vector2.zero;
 
+        // 0 입력으로 리셋
         OutputPointerEventValue(Vector2.zero);
 
-        if(handleRect)
+        // 터치 종료 이벤트 발생
+        touchEndEvent.Invoke();
+
+        if (handleRect)
         {
             SetObjectActiveState(handleRect.gameObject, false);
             UpdateHandleRectPosition(Vector2.zero);
@@ -104,12 +141,12 @@ public class UIVirtualTouchZone : MonoBehaviour, IPointerDownHandler, IDragHandl
 
     Vector2 ApplyInversionFilter(Vector2 position)
     {
-        if(invertXOutputValue)
+        if (invertXOutputValue)
         {
             position.x = InvertValue(position.x);
         }
 
-        if(invertYOutputValue)
+        if (invertYOutputValue)
         {
             position.y = InvertValue(position.y);
         }
@@ -121,5 +158,4 @@ public class UIVirtualTouchZone : MonoBehaviour, IPointerDownHandler, IDragHandl
     {
         return -value;
     }
-    
 }

b4903a4c1e1773d35fc9fcaf0c4feaa7c346b42a
공연경 <dhwm0710@naver.com>
2025-09-10 09:59:37 +0900

    [BE] Refector: 친구 요청시 응답 형식 수정 (S13P21D103-26)
    * refector(friend): 친구 요청시 응답형식 수정

--- api-server/src/main/java/com/ggumtle/ggumtle/friend/application/result/RequestFriendsResult.java
+++ api-server/src/main/java/com/ggumtle/ggumtle/friend/application/result/RequestFriendsResult.java
@@ -1,12 +1,18 @@
 package com.ggumtle.ggumtle.friend.application.result;
 
 public record RequestFriendsResult(
+        Long friendId,
         Long requesterId,
         String requesterNickname,
         Long targetMemberId,
         String targetMemberNickname
 ) {
-    public static RequestFriendsResult of(Long requesterId, String requesterNickname ,Long targetMemberId, String targetMemberNickname) {
-        return new RequestFriendsResult(requesterId, requesterNickname, targetMemberId, targetMemberNickname);
+    public static RequestFriendsResult of(Long friendId, Long requesterId, String requesterNickname ,Long targetMemberId, String targetMemberNickname) {
+        return new RequestFriendsResult(
+                friendId,
+                requesterId,
+                requesterNickname,
+                targetMemberId,
+                targetMemberNickname);
     }
 }
--- api-server/src/main/java/com/ggumtle/ggumtle/friend/presentation/response/RequestFriendResponse.java
+++ api-server/src/main/java/com/ggumtle/ggumtle/friend/presentation/response/RequestFriendResponse.java
@@ -3,12 +3,18 @@ package com.ggumtle.ggumtle.friend.presentation.response;
 import com.ggumtle.ggumtle.friend.application.result.RequestFriendsResult;
 
 public record RequestFriendResponse(
+        Long friendId,
         Long followerId,
         String followerNickname,
         Long followeeId,
         String followeeNickname
 ) {
     public static RequestFriendResponse from(RequestFriendsResult result){
-        return new RequestFriendResponse(result.requesterId(), result.requesterNickname(), result.targetMemberId(),  result.targetMemberNickname());
+        return new RequestFriendResponse(
+                result.friendId(),
+                result.requesterId(),
+                result.requesterNickname(),
+                result.targetMemberId(),
+                result.targetMemberNickname());
     }
 }

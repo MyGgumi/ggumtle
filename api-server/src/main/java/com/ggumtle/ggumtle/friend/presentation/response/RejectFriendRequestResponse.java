482c1def791da21f75347874be56f8f72e037457
공연경 <dhwm0710@naver.com>
2025-09-11 17:00:03 +0900

    [BE] Refecotr: 친구 요청 거절시 응답형식 수정 (closes S13P21D103-29)
    * refecotr(friend): 친구 요청 거절시 응답형식에 friendId 추가

--- api-server/src/main/java/com/ggumtle/ggumtle/friend/presentation/response/RejectFriendRequestResponse.java
+++ api-server/src/main/java/com/ggumtle/ggumtle/friend/presentation/response/RejectFriendRequestResponse.java
@@ -3,10 +3,11 @@ package com.ggumtle.ggumtle.friend.presentation.response;
 import com.ggumtle.ggumtle.friend.application.result.RejectFriendRequestResult;
 
 public record RejectFriendRequestResponse(
+        Long friendId,
         Long followerId,
         String nickname
 ) {
     public static RejectFriendRequestResponse from(RejectFriendRequestResult result) {
-        return new RejectFriendRequestResponse(result.followerId(), result.nickname());
+        return new RejectFriendRequestResponse(result.friendId(),result.followerId(), result.nickname());
     }
 }

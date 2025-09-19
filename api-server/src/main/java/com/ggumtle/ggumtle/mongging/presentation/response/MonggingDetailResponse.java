91749e45f21e6d1825cf7ef5be7f51fcb9feed6e
박기현 <kiryanchi@gmail.com>
2025-09-19 13:40:51 +0900

    [BE] Feat: 몽깅이 상세 조회 (S13P21D103-129)
    * style: import 정리
    
    * feat: 노션 명세서 준수
    
    * refactor: 몽깅이 id 명시
    
    * feat: 몽깅이 상세조회 클래스 id 추가
    
    * feat: 몽깅이 상세 조회

---
+++ api-server/src/main/java/com/ggumtle/ggumtle/mongging/presentation/response/MonggingDetailResponse.java
@@ -0,0 +1,32 @@
+package com.ggumtle.ggumtle.mongging.presentation.response;
+
+import com.ggumtle.ggumtle.mongging.application.result.MonggingDetailResult;
+import lombok.AccessLevel;
+import lombok.Builder;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record MonggingDetailResponse(
+    Long id,
+    String monggingClass,
+    Integer nowLevel,
+    Double nowPercentage,
+    boolean isMaxLevel,
+    Integer afterLevel,
+    Double afterPercentage,
+    Integer needCoin,
+    Integer successPercentage
+) {
+    public static MonggingDetailResponse of(MonggingDetailResult result) {
+        return MonggingDetailResponse.builder()
+            .id(result.monggingId())
+            .monggingClass(result.monggingClassName())
+            .nowLevel(result.nowLevel())
+            .nowPercentage(result.nowPercentage())
+            .isMaxLevel(result.isMaxLevel())
+            .afterLevel(result.afterLevel())
+            .afterPercentage(result.afterPercentage())
+            .needCoin(result.needCoin())
+            .successPercentage(result.successPercentage())
+            .build();
+    }
+}

25ce6968a19ba019db4401af573fafe1c20a74f4
박기현 <kiryanchi@gmail.com>
2025-09-21 22:06:18 +0900

    [BE] Fix: PutItem Buffer Overflow 해결 (S13P21D103-171)
    * fix: Buffer Overflow 해결

--- game-server/src/main/java/com/ggumtle/ggumtle/dream/application/body/PutItemBody.java
+++ game-server/src/main/java/com/ggumtle/ggumtle/dream/application/body/PutItemBody.java
@@ -41,7 +41,7 @@ public record PutItemBody(
         }
 
         buffer.putInt(-1);
-        for (int i = 0; i <= Box.BOX_SIZE; i++) {
+        for (int i = 0; i < Box.BOX_SIZE; i++) {
             buffer.putInt(-1);
         }
 

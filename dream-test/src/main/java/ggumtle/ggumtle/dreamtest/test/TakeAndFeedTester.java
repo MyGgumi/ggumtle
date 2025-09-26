6bb2830ce10e3212d64d663bc7b450791a337f52
정현정 <wgwjh05169@gmail.com>
2025-09-26 13:04:29 +0900

    [BE] Test: 꿈틀이 젤리 먹이기 테스트 수정
    * test: 꿈틀이 젤리 먹이기 테스트에 중단 추가

--- dream-test/src/main/java/ggumtle/ggumtle/dreamtest/test/TakeAndFeedTester.java
+++ dream-test/src/main/java/ggumtle/ggumtle/dreamtest/test/TakeAndFeedTester.java
@@ -32,32 +32,39 @@ public class TakeAndFeedTester {
         sleep(1000);
 
         log.info("꿈틀이 먹이기 테스트 시작");
+
+        log.info("상자 열기");
         monggingClients.getFirst().sendShowBoxMessage(1);
 
         sleep(1000);
 
-        log.info("꿈틀이 파내기 전에 먹이기 시도");
-        monggingClients.getFirst().sendStartFeedMessage(1);
-
-        sleep(1000);
+//        log.info("꿈틀이 파내기 전에 먹이기 시도");
+//        monggingClients.getFirst().sendStartFeedMessage(1);
+//
+//        sleep(1000);
 
         log.info("꿈틀이 파내기");
         monggingClients.getFirst().sendDigUpMessage(1);
 
         sleep(3500);
 
-        log.info("꿈젤리 얻기 전에 먹이기 시도");
-        monggingClients.getFirst().sendStartFeedMessage(1);
-
-        sleep(1000);
-
-        log.info("1번 상자의 0번째 아이템 꺼내기 시도");
-        monggingClients.getFirst().sendTakeItemMessage(1, 0);
-
-        sleep(1000);
+//        log.info("꿈젤리 얻기 전에 먹이기 시도");
+//        monggingClients.getFirst().sendStartFeedMessage(1);
+//
+//        sleep(1000);
+//
+//        log.info("1번 상자의 0번째 아이템 꺼내기 시도");
+//        monggingClients.getFirst().sendTakeItemMessage(1, 0);
+//
+//        sleep(1000);
 
         log.info("꿈틀이 먹이기 시도");
         monggingClients.getFirst().sendStartFeedMessage(1);
+
+        sleep(1000);
+
+        log.info("꿈틀이 먹이기 중단");
+        monggingClients.getFirst().sendStopFeedMessage();
     }
 
     private void sleep(long millis) {

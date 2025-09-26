ca8650542371fff7ef2fde371ced6e2c537fdc0f
정현정 <wgwjh05169@gmail.com>
2025-09-26 13:23:04 +0900

    [BE] Refactor: 몽둥이 공포 스킬 쿨타임 수정
    * refactor: 몽둥이의 공포 스킬 쿨타임을 임의로 1초로 수정

--- game-server/src/main/java/com/ggumtle/ggumtle/dream/domain/player/Mongdung.java
+++ game-server/src/main/java/com/ggumtle/ggumtle/dream/domain/player/Mongdung.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Mongdung extends Player {
     private static final int BASE_MOVE_SPEED = 1_000_000_000;
     private static final int BASE_DAMAGE = 40;
-    private static final int BASE_SCARE_COOL_TIME = 30 * 1000;
+    private static final int BASE_SCARE_COOL_TIME = 1 * 1000;
     private static final int MAX_BURY_COUNT = 3;
     public static final int SIZE = 10000;
     private static final double HIT_SIZE_X = 500;
